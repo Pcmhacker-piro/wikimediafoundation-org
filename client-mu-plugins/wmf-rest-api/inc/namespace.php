7ec82107adaa3564699efd938d550f3fedd72351
K. Adam White <kadamwhite@users.noreply.github.com>
2023-12-01 14:44:45 -0600

    917: Allow dataset CSV endpoints to be accessed without REST authentication

--- client-mu-plugins/wmf-rest-api/inc/namespace.php
+++ client-mu-plugins/wmf-rest-api/inc/namespace.php
@@ -14,6 +14,7 @@ use WP_Error;
  */
 function bootstrap() {
 	add_filter( 'rest_authentication_errors', __NAMESPACE__ . '\\restrict_public_rest_api_access' );
+	add_filter( 'wmf/rest-api/public_api_endpoints', __NAMESPACE__ . '\\allow_dataset_csv_endpoint_access', 10, 2 );
 }
 
 /**
@@ -32,9 +33,18 @@ function restrict_public_rest_api_access( $errors ) {
 		return $errors;
 	}
 
-	// Return an unauthorized response error if the user
-	// does not have editing capabilities.
-	if ( ! current_user_can( 'edit_posts' ) ) {
+	/**
+	 * Filter which API endpoints are allowed to be accessed publicly so that
+	 * specific endpoints can be made publicly accessible as required, such as
+	 * vega-lite CSV endpoints.
+	 *
+	 * @param bool    $is_public    Whether the endpoint is publicly accessible, false by default.
+	 * @param string  $rest_route   Which endpoint is being accessed.
+	 */
+	$is_public_endpoint = apply_filters( 'wmf/rest-api/public_api_endpoints', false, $GLOBALS['wp']->query_vars['rest_route'] ?? '' );
+
+	// Return an unauthorized response error if user does not have editing capabilities.
+	if ( ! current_user_can( 'edit_posts' ) && ! $is_public_endpoint ) {
 		return new WP_Error(
 			'rest_disabled',
 			__( 'You do not have permission to access the REST API.', 'wmf-rest-api' ),
@@ -43,4 +53,22 @@ function restrict_public_rest_api_access( $errors ) {
 	}
 
 	return $errors;
+}
+
+
+/**
+ * Allow CSV dataset endpoints to be accessed without authentication.
+ *
+ * @param bool    $is_public    Whether the endpoint is publicly accessible, false by default.
+ * @param string  $rest_route   Which endpoint is being accessed.
+ * @return bool Filtered public access flag.
+ */
+function allow_dataset_csv_endpoint_access( $is_public, $rest_route = '' ): bool {
+	if ( preg_match( '#wp/v2/.*/datasets/[^.]+\.csv#', $rest_route ) ) {
+		// Permit access to datasets CSV endpoints, which are loaded asyncronously
+		// by the vega-lite visualization plugin.
+		return true;
+	}
+
+	return $is_public;
 }
